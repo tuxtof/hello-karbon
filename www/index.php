ddc8c2bb3b8ae672c74439ba39fb489cf4020608
Cris Tof <reg-github@geo6.net>
2019-02-14 10:54:50 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello Karbon !</title>
+	<title>Hello World!</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
